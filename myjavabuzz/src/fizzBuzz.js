f1ca052c2d9a151b4c3d435912253b017d2572a9
BartJudge <BartJudge@gmail.com>
2016-08-14 11:05:24 +0100

    basic tests now passing

---
+++ myjavabuzz/src/fizzBuzz.js
@@ -0,0 +1,16 @@
+var Javabuzz = function(){};
+
+Javabuzz.prototype.isDivisibleByThree = function(number) {
+  return (number % 3 ===0);
+};
+
+Javabuzz.prototype.isDivisibleByFive = function(number) {
+  return (number % 5 ===0);
+};
+Javabuzz.prototype.isDivisibleByFifteen = function(number) {
+  return (number % 15 ===0);
+};
+
+// Javabuzz.prototype.isDivisibleBythree = function(number) {
+//   return this._isDivisibleBy(number, 3);
+// };
